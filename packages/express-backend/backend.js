9d1456b04d8f0709247dff4986d769e57602e403
Sombaudy <sombaudy109@gmail.com>
2024-02-01 04:36:31 -0800

    asgn 3

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,6 @@
 // backend.js
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -87,6 +88,7 @@ const addUser = (user) => {
 //     }
 // });
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
